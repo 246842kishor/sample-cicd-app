6d193c50cf5f019c5f4fad50b38c7615f724ce69
kishor <kishor.kumar@cognizant.com>
2026-04-16 19:04:07 +0530

    sample-cicd-app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// filepath: c:\Users\246842\sample-cicd-app\Program.cs
+using System;
+
+namespace SampleCicdApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CI/CD World!..............");
+            Console.ReadLine();  // Keeps the app running for demo
+        }
+    }
+}
